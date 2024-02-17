6503e829b49717471bd94b8d01a92c8c2c5bdaaf
adarSonmez <adarsonmez@outlook.com>
2024-02-17 21:23:44 +0300

    Complete abstract repository pattern

---
+++ DataAccess/Repositories/Concrete/EntityFramework/Analytics/EfReportDal.cs
@@ -0,0 +1,10 @@
+using Core.DataAccess.EntityFramework;
+using DataAccess.Context.EntityFramework;
+using DataAccess.Repositories.Abstract.Analytics;
+using Domain.Entities.Analytics;
+
+namespace DataAccess.Repositories.Concrete.EntityFramework.Analytics;
+
+public class EfReportDal : EfEntityRepository<Report, EfDbContext>, IReportDal
+{
+}
---
+++ DataAccess/Repositories/Concrete/EntityFramework/Membership/EfCustomerDal.cs
@@ -0,0 +1,10 @@
+using Core.DataAccess.EntityFramework;
+using DataAccess.Context.EntityFramework;
+using DataAccess.Repositories.Abstract.Membership;
+using Domain.Entities.Membership;
+
+namespace DataAccess.Repositories.Concrete.EntityFramework.Membership;
+
+public class EfCustomerDal : EfEntityRepository<Customer, EfDbContext>, ICustomerDal
+{
+}
