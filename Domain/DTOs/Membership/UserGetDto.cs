a96b232fc5cba971ae56b3ee2088962920c677c2
adarsonmez <adarsonmez@outlook.com>
2024-05-02 21:56:56 +0300

    Role added to get dto

--- Domain/DTOs/Membership/UserGetDto.cs
+++ Domain/DTOs/Membership/UserGetDto.cs
@@ -13,6 +13,8 @@ public class UserGetDto : IDto
     public string PhoneNumber { get; set; } = null!;
 
     public string UseMultiFactorAuthentication { get; set; } = null!;
+    
+    public string Role { get; set; } = null!;
 
     public string EmailVerified { get; set; } = null!;
 
